17d064ccae23c4b0e0a0caf7bcac7cc7c0fe5450
kapoormayank <kapoormayank645@gmail.com>
2026-01-01 13:38:58 +0530

    Interfaces

---
+++ Interfacee/ExactOutput.java
@@ -0,0 +1,9 @@
+
+public class ExactOutput {
+    public static void main(String[] args) {
+        Computer comput=new Laptop();
+        System.out.println(comput.build());
+        System.out.println(comput.compile());
+    }
+}
+
---
+++ Interfacee/Laptop.java
@@ -0,0 +1,11 @@
+
+public class Laptop implements Computer {
+    @Override
+    public String build(){
+        return "Laptop is build a program";
+    }
+    @Override
+    public String compile(){
+        return "Laptop Compiler run completely";
+    }
+}
