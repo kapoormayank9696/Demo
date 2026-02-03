f14afba6226badcf4319bcb3788fde3f19307c8d
kapoormayank <kapoormayank645@gmail.com>
2026-02-03 23:54:22 +0530

    Data Structure And Algorithms

--- DSA/Hashing/HashMapping.java
+++ DSA/Hashing/HashMapping.java
@@ -1,27 +1,52 @@
 // HashMap Algorithm Implementation In Java 
 
 import java.util.HashMap;
-import java.util.Scanner;
-
+import java.util.Map;
+import java.util.Set;
 public class HashMapping {
 
     // Main function
     @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
-        HashMap<Integer,String> map = new HashMap<>();
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter number of terms: ");
-        int n=sc.nextInt();
-        System.out.print("Enter elements: ");
-        for(int i=0;i<n;i++){
-            int key=sc.nextInt();
-            String value=sc.nextLine();
-            map.put(key, value);
-        }
-        
-        // Print the hash map
-        System.out.println("Print hash map data: "+map);
+        HashMap<String,Integer> map = new HashMap<>();
+        map.put("India",190);
+        map.put("China", 180);
+        map.put("Russia", 100);
+        map.put("America", 90);
+        map.put("Irak",72);
+        map.put("Colombia", 80);
+        map.put("Iran", 79);
+        map.put("Spain",77);
 
-        sc.close();
+        // Print the hash map
+        System.out.println("\nPrint hash map data: "+map);
+
+        // Search operation
+        if (map.containsKey("china")) {
+            System.out.println("Yes,This key exist in the hash map.....");
+        }else{
+            System.out.println("No,This key doesn't exist in the hash map!!!");
+        }
+
+        // Print the value which exist in the hash map
+        System.out.println("Value of exist key in the hash map: "+map.get("India")); // Key exists
+        System.out.println("Value of doesn't exist key in the hash map: "+map.get("k")); // key doesn't exists
+
+        // Iteration Operation in hash map (Entery Set Method)
+        for(Map.Entry<String,Integer> e : map.entrySet()){
+            System.out.print(e.getKey()+"="+ e.getValue()+" ");
+        }
+        System.out.print("\n");
+
+        // Method to print all keys with his values(Key Set Method)
+        Set<String> keys = map.keySet();
+        for(String key: keys){
+            System.out.print(key+"="+map.get(key)+" ");
+        }
+
+        // Remove the key-value from the hash map
+        map.remove("China");
+        System.out.println("\nPrint hash map: "+map);
     }
 }
+
