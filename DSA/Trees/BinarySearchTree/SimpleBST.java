0ae1e1b112f03d1e9c4a7b5cf0a8dc7dbd458530
kapoormayank <kapoormayank645@gmail.com>
2026-02-07 18:44:09 +0530

    Data Structure And Algorithms

---
+++ DSA/Trees/BinarySearchTree/SimpleBST.java
@@ -0,0 +1,34 @@
+// Simple Binary Search Tree Algorithm Implementation In Java
+
+public class SimpleBST {
+    // Node class
+    static class Node{
+        // Deafult Access Modifier And Data Members
+        int data;
+        Node left;
+        Node right;
+        // Parameterized Constructor
+        public Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        Node root=new Node(23);
+        root.left=new Node(16);
+        root.right=new Node(30);
+        root.left.left=new Node(2);
+        root.left.right=new Node(20);
+        root.right.left=new Node(28);
+        root.right.right=new Node(35);
+
+        System.out.println("    "+root.data);
+        System.out.println("   /  \\");
+        System.out.println("  "+root.left.data+"   "+root.right.data);
+        System.out.println(" / \\ "+"  / \\");
+        System.out.println(root.left.left.data+"   "+root.left.right.data+" "+root.right.left.data+" "+root.right.right.data);
+    }
+}
