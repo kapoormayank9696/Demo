16eb2aed50aeff22dccbfe57eabdd73d158d600c
kapoormayank <kapoormayank645@gmail.com>
2026-02-12 20:55:34 +0530

    Data Structure And Algorithms

--- DSA/Trees/BinaryTree/DiameterOf_BT2.java
+++ DSA/Trees/BinaryTree/DiameterOf_BT2.java
@@ -64,6 +64,7 @@ public class DiameterOf_BT2 {
         TreeInfo myInfo=new TreeInfo(myHeight, mydiam);
         return myInfo;
     }
+    
     // Main function
     public static void main(String[] args) {
         int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
