8f3d133d36caeee23406ee52e819eb5428f94aaf
kapoormayank <kapoormayank645@gmail.com>
2026-02-22 20:35:27 +0530

    LeetCode Problmes Solution

---
+++ LeetCode_Problems/Solution69.java
@@ -0,0 +1,15 @@
+// LeetCode Problem 69: Sqrt(x)
+
+public class Solution69{
+    // Function to find the square root of a number
+    public static int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        int x=8;
+        System.out.println("The square root of "+x+" is: "+mySqrt(x));
+    }
+}
+
