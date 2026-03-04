510f037d73b0f7ffe662d1fee5f4d64e5b0d0d37
kapoormayank <kapoormayank645@gmail.com>
2026-03-04 11:38:16 +0530

    Data Structure And Algorithms

---
+++ DSA/LinkedList/CircularLinkedList/Insertion/InsertAtEnd.java
@@ -0,0 +1,94 @@
+// Insert At End Algorithm Implementation In Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class InsertAtEnd {
+    // Node class of Linked List
+    @SuppressWarnings("unused")
+    static class Node {
+        // Default Access Modifier And Data Members
+        int data;
+        Node next;
+        // Parameterized Constructor
+        public Node(int data) {
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    Node head=null;
+    // Function to insert the nodes in Linked List
+    public void insert(int data) {
+        Node newNode=new Node(data);
+        if(head == null) {
+            head=newNode;
+            newNode.next=head; // Point to itself
+            return;
+        }
+        Node temp=head;
+        // Traverse to the last node
+        while(temp.next != head) {
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        // Making the last node point to head to maintain circular nature
+        newNode.next=head;
+    }
+
+    // Functio to insert the node in circular linked list at end point
+    public void insertAtEnd(int data) {
+        Node newNode=new Node(data);
+        if(head == null) {
+            head=newNode;
+            newNode.next=head; // Point to itself
+            return;
+        }
+        Node temp=head;
+        // Traverse to the last node
+        while(temp.next != head) {
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        // Making the last node point to head to maintain circular nature
+        newNode.next=head;
+    }
+
+    // Function to display the linked list
+    public void display() {
+        if(head == null) {
+            System.out.println("Empty Circular Linked List....");
+            return;
+        }
+        Node temp=head;
+        while(temp != null) {
+            System.out.print(temp.data+"<-->");
+            temp=temp.next;
+            if(temp == head){
+                break;
+            }
+        }
+        System.out.println("(head)");
+    }
+
+    // Main function
+    public static void main(String[] args) throws Exception{
+        @SuppressWarnings("unused")    
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter number of terms: ");
+        int n=Integer.parseInt(br.readLine());
+        InsertAtEnd list=new InsertAtEnd();
+        System.out.print("Enter nodes in circular linked list: ");
+        for(int i=0;i < n;i++) {
+            int value=Integer.parseInt(br.readLine());
+            list.insert(value);
+        }
+        System.out.print("Display Circular Linked List: ");
+        list.display();
+        System.out.print("Enter node element insert at end position: ");
+        int k=Integer.parseInt(br.readLine());
+        list.insertAtEnd(k);
+        System.out.print("After insert node at last position: ");
+        list.display();
+        br.close();
+    }
+}
