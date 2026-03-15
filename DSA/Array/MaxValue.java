6445087c34590fb332b8f7e2ba8c3f558271b02e
kapoormayank <kapoormayank645@gmail.com>
2026-03-15 17:55:48 +0530

    Data Structure And Algorithms

---
+++ DSA/Array/MaxValue.java
@@ -0,0 +1,40 @@
+// Find The Maximum Value From An Array Algorithm Implementation In Java
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MaxValue {
+    // Function to find the maximum value from an array
+    public static int maxValue(int[] arr) {
+        int max= arr[0];
+        for(int num:arr) {
+            if(num > max){
+                max=num;
+            }
+        }
+        return max;
+    }
+
+    // Function to print array elements
+    public static void printArray(int[] arr) {
+        for(int num:arr) {
+            System.out.print(num+" ");
+        }
+        System.out.print("\n");
+    }
+
+    // Main function
+    public static void main(String[] args) throws Exception{
+        try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in));) {
+            System.out.print("Enter size of an array: ");
+            int n=Integer.parseInt(br.readLine());
+            int[] arr=new int[n];
+            System.out.print("Enter elements: ");
+            for(int i=0;i<n;i++) {
+                arr[i]=Integer.parseInt(br.readLine());
+            }
+            System.out.print("Display array: ");
+            printArray(arr);
+            System.out.println("Maximum value from an array: "+maxValue(arr));
+        }
+    }    
+}
