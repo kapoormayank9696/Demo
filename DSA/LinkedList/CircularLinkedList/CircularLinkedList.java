388df5f9e1802441a28d3461fa50a3c5829120d8
kapoormayank <kapoormayank645@gmail.com>
2026-04-11 19:16:20 +0530

    Data Structure And Algorithms

--- DSA/LinkedList/CircularLinkedList/CircularLinkedList.java
+++ DSA/LinkedList/CircularLinkedList/CircularLinkedList.java
@@ -9,19 +9,132 @@ public class CircularLinkedList{
         }
     }
     Node head=null;
-    Node tail=null;
+
+    // Insertion nodes in circular linked list
     public void insert(int data){
         Node newNode = new Node(data);
         if(head==null){
             head=newNode;
-            tail=newNode;
-            tail.next=head;
+            newNode.next=head;
         }else{
-            tail.next=newNode;
-            tail=newNode;
-            tail.next=head;
+            Node temp=head;
+            while(temp.next != head){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+            newNode.next=head;
         }
     }
+
+    // Insertion at the beginning of circular linked list
+    public void insertAtBeginning(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head=newNode;
+            newNode.next=head;
+        }else{
+            Node temp=head;
+            while(temp.next != head){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+            newNode.next=head;
+            head=newNode;
+        }
+    }
+
+    // Insertion at a specific position in circular linked list
+    public void insertAtPosition(int data, int position){
+        Node newNode = new Node(data);
+        if(position==1){
+            insertAtBeginning(data);
+            return;
+        }
+        Node temp=head;
+        for(int i=1; i<position-1; i++){
+            temp=temp.next;
+            if(temp==head){
+                System.out.println("Position out of bounds");
+                return;
+            }
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+
+    // Insertion at the end of circular linked list
+    public void insertAtEnd(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head=newNode;
+            newNode.next=head;
+        }else{
+            Node temp=head;
+            while(temp.next != head){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+            newNode.next=head;
+        }
+    }
+
+    // Deletion at the beginning of circular linked list
+    public void deleteAtBeginning(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        if(head.next==head){
+            head=null;
+            return;
+        }
+        Node temp=head;
+        while(temp.next != head){
+            temp=temp.next;
+        }
+        temp.next=head.next;
+        head=head.next;
+    }
+
+    // Deletion at a specific position in circular linked list
+    public void deleteAtPosition(int position){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        if(position==1){
+            deleteAtBeginning();
+            return;
+        }
+        Node temp=head;
+        for(int i=1; i<position-1; i++){
+            temp=temp.next;
+            if(temp.next==head){
+                System.out.println("Position out of bounds");
+                return;
+            }
+        }
+        temp.next=temp.next.next;
+    }
+
+    // Deletion at the end of circular linked list
+    public void deleteAtEnd(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        if(head.next==head){
+            head=null;
+            return;
+        }
+        Node temp=head;
+        while(temp.next.next != head){
+            temp=temp.next;
+        }
+        temp.next=head;
+    }
+    
+    // Display nodes in circular linked list
     public void display(){
         Node temp=head;
         do{
@@ -30,7 +143,9 @@ public class CircularLinkedList{
         }
         while(temp != head);
         System.out.println("Back to head <->"+head.data);
-    } 
+    }
+    
+    // Main function
     public static void main(String[] args){
         CircularLinkedList list = new CircularLinkedList();
         list.insert(5);
