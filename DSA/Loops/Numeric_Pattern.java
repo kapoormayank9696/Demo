b26c46d1181c88f09752e679707ddeb030911611
kapoormayank <kapoormayank645@gmail.com>
2026-04-22 23:07:30 +0530

    Data Structure And Algorithms

---
+++ DSA/Loops/Numeric_Pattern.java
@@ -0,0 +1,18 @@
+// Numeric Pattern Algorithm Implementation In Java
+import java.util.Scanner;
+public class Numeric_Pattern {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
