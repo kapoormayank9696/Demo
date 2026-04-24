1c0c674cdcbc5443e7bf60bf9ab821632da54dc5
kapoormayank <kapoormayank645@gmail.com>
2026-04-24 22:30:05 +0530

    Data Structure And Algorithms

---
+++ DSA/Loops/Sum_Of_Numbers.java
@@ -0,0 +1,16 @@
+// Sum Of N Numbers Implementation In Java
+
+import java.util.Scanner;
+public class Sum_Of_Numbers {
+    // Main function
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of first " + n + " numbers is: " + sum);
+    }
+}
