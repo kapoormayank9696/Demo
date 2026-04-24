bafb33b389d900dec2de700b2f6f5a59a74c0c02
kapoormayank <kapoormayank645@gmail.com>
2026-04-24 22:27:07 +0530

    Data Structure And Algorithms

--- DSA/Loops/Pyramid_Pattern.java
+++ DSA/Loops/Pyramid_Pattern.java
@@ -1,6 +1,7 @@
 // Pyramid Pattern Implementation In Java
 
 public class Pyramid_Pattern {
+    // Main function
     public static void main(String[] args) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
