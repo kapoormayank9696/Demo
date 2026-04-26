385af207f42cefa8e9ecfc00e28165713946dcc6
kapoormayank <kapoormayank645@gmail.com>
2026-04-26 20:18:41 +0530

    Data Structure And Algorithms

---
+++ DSA/Loops/Factorial.java
@@ -0,0 +1,16 @@
+// Factorial Of A Number Algorithm Implementation In Java
+import java.util.Scanner;
+public class Factorial {
+    // Main function
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i; // fact = fact * i
+        }
+        System.out.println("Factorial of " + n + " is: " + fact);
+        sc.close();
+    }
+}
