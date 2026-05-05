268d40ebab29028a3a9762d007b9d66a22702b58
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-05 14:48:53 +0530

    Data Structure And Algorithms

---
+++ DSA/Loops/LCM.java
@@ -0,0 +1,31 @@
+// LCM ALgorithms Implementation In Java
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class LCM {
+    // Function to find the lcm of two numbers
+    public static int lcm(int a,int b) {
+        int lcm;
+        int k=(a>b)?a:b;
+        for(int i=k;;i+=k) {
+            if(i%a == 0 && i%b == 0) {
+                lcm=i;
+                break;
+            }
+        }
+        return lcm;
+    }
+
+    // Main function
+    public static void main(String[] args) throws Exception{
+        try(BufferedReader reader=new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Input 1st number for LCM: ");
+            int n=Integer.parseInt(reader.readLine());
+            System.out.print("Input 2nd number for LCM: ");
+            int m=Integer.parseInt(reader.readLine());
+            int result=lcm(n,m);
+            System.out.println("The LCM of "+" and "+" is : "+result);
+        }
+    }
+}
+
+
