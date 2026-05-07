05c8e21116521029a9da648de57d7df0ada53888
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-07 21:19:57 +0530

    Data Structure And Algorithms

---
+++ DSA/Loops/HexaDecimal_To_Decimal.java
@@ -0,0 +1,19 @@
+// HexaDecimal To Decimal Algorithm Implementation In Java
+import java.io.*;
+public class HexaDecimal_To_Decimal {
+    // Function to Convert Hexa Decaiml To Decimal
+    public static void hexaToDecimal(String hex) {
+        int result = Integer.parseInt(hex, 16);
+        System.out.println("The equivalent Decimal Number : "+result);
+    }
+
+    // Main function
+    public static void main(String[] args) throws Exception{
+        try(BufferedReader reader=new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Input  any Hexa-Decimal number: ");
+            String n=reader.readLine();
+            hexaToDecimal(n);
+        }
+    }
+}
+
