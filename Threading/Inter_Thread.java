de3057d094ab2d645aa94d667e79f6591ef9b2fa
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-14 20:58:37 +0530

    Thread File Create Successfully

---
+++ Threading/Inter_Thread.java
@@ -0,0 +1,14 @@
+// Inter Thread Communication Example Implementation In Java
+
+public class Inter_Thread {
+    // Main function
+    public static void main(String[] args) {
+        // Create Threads
+        Thread t1 = new Thread (() -> {
+        });
+        Thread t2 = new Thread (() -> {
+        });
+        Thread t3 = new Thread (() -> {
+        });
+    }
+}
