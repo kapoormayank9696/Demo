7a6ffea3041138e7c2332d72a6491726ebcb8726
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-17 12:18:25 +0530

    LeetCode Problems Solution

---
+++ LeetCode_Problems/Solution110.java
@@ -0,0 +1,90 @@
+// LeetCode Problem 110: Balanced Binary Tree
+
+public class Solution110 {
+    // Node class for the binary tree
+    public static class TreeNode {
+        // Data members
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        // Parameterized Constructor
+        TreeNode(int x) {
+            this.val = x;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Function to insert the nodes in the binary tree
+    public static TreeNode insert(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        // Left Subtree
+        if (val < root.val) {
+            root.left = insert(root.left, val);
+        } 
+        // Right Subtree
+        else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    // Function to print the binary tree pre order
+    public static void printPreOrder(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + " ");
+            printPreOrder(root.left);
+            printPreOrder(root.right);
+        }
+    }
+
+    // Function to check the height of the tree and determine if it is balanced
+    public static int checkHeight(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftHeight=checkHeight(root.left);
+        
+        if(leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight=checkHeight(root.right);
+
+        if(rightHeight == -1) {
+            return -1;
+        }
+
+        if(Math.abs(leftHeight-rightHeight) > 1) {
+            return -1;
+        }
+
+        int myHeight=Math.max(leftHeight,rightHeight)+1;
+        return myHeight;
+    }
+
+    // Function to determine if the binary tree is balanced
+    public static boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        // Integer[] values = {9,3, 20, null, null, 15, 7};
+        Integer[] values = {1,2,2,3,null,null,3,4,null,null,4};
+        TreeNode root = null;
+        for (Integer val : values) {
+            if (val != null) {
+                root = insert(root, val);
+            }
+        }
+        System.out.println("Pre-order Traversal of the Binary Tree:");
+        printPreOrder(root);
+        System.out.println("\nIs the binary tree balanced? " + isBalanced(root));
+    }
+}
+
