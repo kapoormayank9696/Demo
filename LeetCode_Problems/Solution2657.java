a92ce33e598981717cbbb1867fc7b5941b2e249c
kapoormayank9696 <kapoormayank645@gmail.com>
2026-05-21 12:52:44 +0530

    LeetCode Problems Solution

---
+++ LeetCode_Problems/Solution2657.java
@@ -0,0 +1,41 @@
+// LeetCode Problem 2657: Find the Prefix Common Array of Two Arrays
+public class Solution2657 {
+    
+    // Class to solve the problem
+    public static class Solution {
+        public int[] findThePrefixCommonArray(int[] A, int[] B) {
+            int n = Math.min(A.length, B.length);
+            int[] frequency = new int[n+1];
+            int[] ans = new int[n];
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+
+                frequency[A[i]]++;
+                if (frequency[A[i]] == 2) {
+                    count ++;
+                }
+
+                frequency[B[i]]++;
+                if (frequency[B[i]] == 2) {
+                    count ++;
+                }
+
+                ans[i] = count;
+            }
+            return ans;
+        }   
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        int[] A = {1, 3, 2, 4};
+        int[] B = {3, 1, 2, 4};
+        Solution solution = new Solution();
+        int[] result = solution.findThePrefixCommonArray(A, B);
+        System.out.print("Prefix Common Array: ");
+        for(int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
