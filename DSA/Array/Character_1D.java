bd109888c6bfb3ef0f13f74662009be22625c19c
kapoormayank9696 <kapoormayank645@gmail.com>
2026-06-09 22:25:09 +0530

    Data Structure And Algorithms

---
+++ DSA/Array/Character_1D.java
@@ -0,0 +1,13 @@
+// Character 1-D Array Algorithm Implementation In Java
+
+public class Character_1D {
+    // Main function
+    public static void main(String[] args) {
+        char[] charArray = {'H', 'e', 'l', 'l', 'o'};     
+        System.out.println("\nCharacter 1-D Array:");
+        for (char c : charArray) {
+            System.out.print(c + " ");
+        }
+    }
+}
+
