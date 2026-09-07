f569c024800a535a478dba794093472efebea5f8
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-07 18:31:57 +0530

    Data Structure And Algorithms

--- DSA/Graph/RottenOrgane.java
+++ DSA/Graph/RottenOrgane.java
@@ -1,9 +1,21 @@
-
+// Rotten Oranges Algorithm Implement In Java
 
 public class RottenOrgane {
 
+    // Graph Class
+    public static class Graph {
+
+    }
+
     // Main function
     public static void main(String[] args) {
+        int[][] grid = {
+            {2, 1, 1},
+            {1, 1, 0},
+            {0, 1, 1}
+        };
+
+        Graph graph = new Graph();
         
     }    
 }
