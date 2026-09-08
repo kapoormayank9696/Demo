361375853300bf99d52034369117fb9ca06606da
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-08 12:06:31 +0530

    Data Structure And Algorithms

--- DSA/Graph/RottenOrgane.java
+++ DSA/Graph/RottenOrgane.java
@@ -1,10 +1,35 @@
 // Rotten Oranges Algorithm Implement In Java
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RottenOrgane {
 
     // Graph Class
     public static class Graph {
 
+        // Orange Rotting Function
+        public static int orangesRotting(int[][] grid) {
+            int rows = grid.length;
+            int cols = grid[0].length;
+
+            Queue<int[]> queue = new LinkedList<>();
+            boolean[][] visited = new boolean[rows][cols];
+
+            queue.add(new int[]{0, 0}); // Add the initial rotten orange position
+            visited[0][0] = true;
+
+            while(queue.size() > 0) {
+                int u =queue.poll()[0];
+                for(int neighbor: grid[u]) {
+                    if(!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        queue.add(new int[]{neighbor});
+                    }
+                }
+            }
+            return -1;
+        }
     }
 
     // Main function
@@ -15,7 +40,9 @@ public class RottenOrgane {
             {0, 1, 1}
         };
 
-        Graph graph = new Graph();
-        
+        Graph g = new Graph();
+        int result = g.orangesRotting(grid);
+
+        System.out.println("Minimum minutes: " + result);
     }    
 }
