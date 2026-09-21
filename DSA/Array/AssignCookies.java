e1ecc345a631154c17495450eb38cfce52a37902
kapoormayank9696 <kapoormayank645@gmail.com>
2026-09-21 11:21:40 +0530

    Data Structure And Algorithms

--- DSA/Array/AssignCookies.java
+++ DSA/Array/AssignCookies.java
@@ -6,15 +6,18 @@ public class AssignCookies {
 
         // QuickSort implementation to sort the arrays
         public void quickSort(int[] nums,int low,int high) {
+
             if(low< high) {
                 int pi = partition(nums,low,high);
                 quickSort(nums,low,pi-1);
                 quickSort(nums,pi+1,high);
             }
+            
         }
 
         // Partition function used in QuickSort
         public int partition(int[] nums,int low,int high) {
+
             int pivot = nums[high];
             int i = low - 1;
 
@@ -26,15 +29,18 @@ public class AssignCookies {
                     nums[j] = temp;
                 }
             }
+
             i++;
             int temp = nums[i];
             nums[i] = nums[high];
             nums[high] = temp;
+
             return i;
         }
         
         // Function to find the maximum number of content children
         public int findContentChildren(int[] g, int[] s) {
+
             quickSort(g,0,g.length-1);
             quickSort(s,0,s.length-1);
             
